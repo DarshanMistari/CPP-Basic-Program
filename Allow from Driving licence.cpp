a36f5ce941be20741f5d4a8101b02c16232580ad
DarshanMistari <mistaridarshan2@gmail.com>
2024-02-24 22:02:59 +0530

    [ADDED]:Darshan - CPP code for allow for Driving License

---
+++ Allow from Driving licence.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int age;
+	
+	cout<<"Enter the age :"<<endl;
+	cin>>age;
+	
+	if(age>=18)
+	{
+		cout<<"You are allow from Driving Licence";
+	}
+	else
+	{
+		cout<<"You are not allow from Driving Licence";
+	}
+	return 0;
+}
