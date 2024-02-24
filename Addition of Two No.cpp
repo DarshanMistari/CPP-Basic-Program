038ab7ef9f41fe869a214d7abd4aaa194c396c87
DarshanMistari <mistaridarshan2@gmail.com>
2024-02-24 21:46:41 +0530

    [ADDED]:Darshan - CPP code for Addition of Two No

---
+++ Addition of Two No.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int num1 ,num2 , sum;
+	
+	cout<<"Enter the First Number :"<<endl;
+	cin>>num1;
+	cout<<"Enter the Second Number :"<<endl;
+	cin>>num2;
+	sum = num1 + num2;
+	cout<<"Addition: "<<sum;
+	
+	return 0;
+}
