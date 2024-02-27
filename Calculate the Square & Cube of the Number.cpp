6f09ad3fedbcfd45f90fb0cf0822989f81ad5e64
DarshanMistari <mistaridarshan2@gmail.com>
2024-02-27 21:59:05 +0530

    [ADDED]:Darshan - cpp code For Calculate the Square and Cube of Number

---
+++ Calculate the Square & Cube of the Number.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int num,squ,cube;
+	cout<<"Enter the Number:";
+	cin>>num;
+	
+	squ=num*num;
+	cout<<endl<<"Square of the Number is:"<<squ<<endl;
+	
+	cube=num*num*num;
+	cout<<endl<<"Cube of the Number is :"<<cube;
+	
+	return 0;
+}
