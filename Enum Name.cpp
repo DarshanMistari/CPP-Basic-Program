d4b435a4d70ab6222431429bc0c8c93be309a899
DarshanMistari <mistaridarshan2@gmail.com>
2024-03-01 22:26:51 +0530

    [ADDED]:Darshan - Cpp code for Enum

---
+++ Enum Name.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	enum name
+	{
+		Darshan,Kalpesh,Kunal,Sagar,Nitin,Manoj
+	};
+	
+	cout<<Darshan<<endl;
+	name str=Nitin;
+	cout<<str;
+	
+}
