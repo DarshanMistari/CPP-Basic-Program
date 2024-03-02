9fe7eeb5ebaee84242d5b84c6146c65e858756f0
DarshanMistari <mistaridarshan2@gmail.com>
2024-03-02 22:48:38 +0530

    [ADDED]:Darshan - Cpp Code for Positive and Negative Number

---
+++ Positive and Negative Number.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int num;
+	
+	cout<<"Enter the Number:";
+	cin>>num;
+	
+	if(num>=0)
+	{
+		cout<<num<<" is the Positive Number";
+	}
+	else
+	{
+		cout<<num<<" is the Negative Number";
+	}
+	return 0;
+}
